13bf4c7ddaa35a7ce567e0969010b363a3bd2fd0
antonio.marcos <antonio.malves.lima@gmail.com>
2020-11-04 15:33:01 -0300

    melhorando mensagem de erro para as validacoes dos campos.

--- desafio/src/main/java/com/github/antoniomalves/desafio/config/RestExceptionHandler.java
+++ desafio/src/main/java/com/github/antoniomalves/desafio/config/RestExceptionHandler.java
@@ -1,28 +1,66 @@
 package com.github.antoniomalves.desafio.config;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @ControllerAdvice
-public class RestExceptionHandler {
-
-	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<?> handleResourceNotFoundException(IllegalArgumentException illegalArgumentException){
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
-		DetalhesExcecao exceptionDetail = new DetalhesExcecao(
-				"Parâmetro de entrada incorreto. Verifique os valores enviados na requisição.",
-				HttpStatus.BAD_REQUEST.value(),
-				illegalArgumentException.getClass().getName(),
-				new Date().getTime(),
-				illegalArgumentException.getMessage()
-				);
+		List<Erro> erros = criarListaDeErro(ex.getBindingResult(), ex);
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> criarListaDeErro(BindingResult bindingResult, Exception ex){
+		List<Erro> erros = new ArrayList<>();
+		
+		for(FieldError fieldError : bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		}
+		
+		return erros;
+	}
+	
+	public static class Erro{
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+		
+		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
 
-		return new ResponseEntity<>(exceptionDetail, HttpStatus.BAD_REQUEST);
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+		
 	}
 	
 }
